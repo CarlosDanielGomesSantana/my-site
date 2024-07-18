a84c77e62bcac3e7587e99e9e0e8c46be351753d
Carlos Daniel Gomes Santana <carlos.daniel.programmer@gmail.br>
2024-07-18 09:31:32 -0300

    deploy (commit: cb60743a3185e3414d119e450a4f0b960cad59bf)

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m "deploy (commit: $COMMIT)"
 
 # if you are using HTTPS/2FA authentication
 # git push -f https://github.com/<USER-NAME>/<REPO>.git master:gh-pages
-git push -f https://github.com/carlosdanielgomessantana/my-site.git master:gh-pages
+git push -f master
 
 cd ..
